5322a5ea619eb30d8373410f1933c64df0271a55
Anna <basbassa@gmail.com>
2023-04-27 19:32:19 +0200

    try without css

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,25 +1,7 @@
 export function fetchCountries(name) {
-  const fields = 'name.official;flag;population;region;capital';
-  const url = `https://restcountries.com/v3.1/name/${name}?fields=${fields}`;
+  const url = `https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`;
   return fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Could not fetch countries');
-      }
-      return response.json();
-    })
-    .then(data => {
-      const countries = data.map(country => ({
-        name: country.name.official,
-        capital: country.capital[0],
-        population: country.population,
-        flag: country.flags.svg,
-        region: country.region,
-      }));
-      return countries;
-    })
-    .catch(error => {
-      console.error('Error fetching countries:', error);
-      throw error;
-    });
+    .then(response => response.json())
+    .then(data => data)
+    .catch(error => console.log(error));
 }
--- src/index.js
+++ src/index.js
@@ -1,69 +1,70 @@
-export { fetchCountries } from './fetchCountries.js';
+import { fetchCountries } from './fetchCountries.js';
 
-const searchBox = document.getElementById('search-box');
-const regionSelect = document.getElementById('region-select');
+const searchBox = document.querySelector('#search-box');
+const regionSelect = document.querySelector('#region-select');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
+function displayCountryList(countries) {
+  countryList.innerHTML = '';
+  countries.forEach(country => {
+    const li = document.createElement('li');
+    li.classList.add('country');
+    li.innerHTML = `
+      <div class="flag">
+        <img src="${country.flags.svg}" alt="${country.name.official} Flag" />
+      </div>
+      <div class="details">
+        <h2 class="country-name">${country.name.official}</h2>
+        <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
+        <p><strong>Region:</strong> ${country.region}</p>
+        <p><strong>Capital:</strong> ${country.capital[0]}</p>
+      </div>
+    `;
+    li.addEventListener('click', () => displayCountryInfo(country));
+    countryList.appendChild(li);
+  });
+}
+
+function displayCountryInfo(country) {
+  countryInfo.innerHTML = '';
+  const div = document.createElement('div');
+  div.classList.add('details');
+  div.innerHTML = `
+    <button class="back-btn">Back</button>
+    <div class="flag">
+      <img src="${country.flags.svg}" alt="${country.name.official} Flag" />
+    </div>
+    <div class="info">
+      <h2 class="country-name">${country.name.official}</h2>
+      <p><strong>Native Name:</strong> ${country.name.nativeName}</p>
+      <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
+      <p><strong>Region:</strong> ${country.region}</p>
+      <p><strong>Subregion:</strong> ${country.subregion}</p>
+      <p><strong>Capital:</strong> ${country.capital[0]}</p>
+      <p><strong>Top Level Domain:</strong> ${country.topLevelDomain}</p>
+      <p><strong>Currencies:</strong> ${country.currencies.map(currency => currency.name).join(', ')}</p>
+      <p><strong>Languages:</strong> ${country.languages.map(language => language.name).join(', ')}</p>
+    </div>
+  `;
+  const backBtn = div.querySelector('.back-btn');
+  backBtn.addEventListener('click', () => countryInfo.innerHTML = '');
+  countryInfo.appendChild(div);
+}
+
 searchBox.addEventListener('input', () => {
   const name = searchBox.value.trim();
-  const region = regionSelect.value;
-
-  if (name.length > 0) {
-    fetchCountries(name, region)
-      .then(countries => {
-        renderCountryList(countries);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  } else {
-    countryList.innerHTML = '';
-    countryInfo.innerHTML = '';
+  if (name !== '') {
+    fetchCountries(name)
+      .then(data => displayCountryList(data))
+      .catch(error => console.log(error));
   }
 });
 
 regionSelect.addEventListener('change', () => {
-  const name = searchBox.value.trim();
   const region = regionSelect.value;
-
-  if (name.length > 0) {
-    fetchCountries(name, region)
-      .then(countries => {
-        renderCountryList(countries);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  } else {
-    countryList.innerHTML = '';
-    countryInfo.innerHTML = '';
-  }
-});
-
-function renderCountryList(countries) {
-  const html = countries
-    .map(
-      country => `
-        <li class="country-list__item" data-name="${country.name}">
-          <img src="${country.flag}" alt="${country.name} flag" />
-          <div>
-            <h2>${country.name}</h2>
-            <p>
-              <strong>Population:</strong> ${country.population.toLocaleString()}
-            </p>
-            <p>
-              <strong>Region:</strong> ${country.region}
-            </p>
-            <p>
-              <strong>Capital:</strong> ${country.capital}
-            </p>
-          </div>
-        </li>
-      `
-    )
-    .join('');
-
-  countryList.innerHTML = html;
-  countryInfo.innerHTML = '';
-}
+  if (region !== '') {
+    const url = `https://restcountries.com/v3.1/region/${region}?fields=name.official,capital,population,flags.svg,languages`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => displayCountryList(data))
