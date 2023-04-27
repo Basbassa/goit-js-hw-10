5580c13a2becd94b0a99b38a2207dc1f9c635b73
Anna <basbassa@gmail.com>
2023-04-27 19:34:43 +0200

    Revert "try without css"
    This reverts commit 5322a5ea619eb30d8373410f1933c64df0271a55.

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,25 @@
 export function fetchCountries(name) {
-  const url = `https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`;
+  const fields = 'name.official;flag;population;region;capital';
+  const url = `https://restcountries.com/v3.1/name/${name}?fields=${fields}`;
   return fetch(url)
-    .then(response => response.json())
-    .then(data => data)
-    .catch(error => console.log(error));
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Could not fetch countries');
+      }
+      return response.json();
+    })
+    .then(data => {
+      const countries = data.map(country => ({
+        name: country.name.official,
+        capital: country.capital[0],
+        population: country.population,
+        flag: country.flags.svg,
+        region: country.region,
+      }));
+      return countries;
+    })
+    .catch(error => {
+      console.error('Error fetching countries:', error);
+      throw error;
+    });
 }
